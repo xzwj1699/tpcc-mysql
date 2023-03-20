b1b904e7e505e670550024212d9319063d5a6afb
xzwj1699 <54903746+xzwj1699@users.noreply.github.com>
2023-03-20 21:51:20 +0800

    Update load.c

--- src/load.c
+++ src/load.c
@@ -231,8 +231,8 @@ main(argc, argv)
 			       "INSERT INTO customer values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, 10.0, 1, 0,?)",
 			       76) ) goto Error_SqlCall_close;
 	if( mysql_stmt_prepare(stmt[5],
-			       "INSERT INTO history values(?,?,?,?,?,?,?,?)",
-			       43) ) goto Error_SqlCall_close;
+			       "INSERT INTO history values(null,?,?,?,?,?,?,?,?)",
+			       48) ) goto Error_SqlCall_close;
 	if( mysql_stmt_prepare(stmt[6],
 			       "INSERT INTO orders values(?,?,?,?,?,NULL,?, 1)",
 			       46) ) goto Error_SqlCall_close;
